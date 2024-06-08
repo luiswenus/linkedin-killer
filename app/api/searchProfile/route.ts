ece9db7a7ca94efe9ff4165f7be391c813eff886
Matthias Wolf <matze.wolf@tum.de>
2024-06-08 19:18:07 +0200

    semantic search

--- app/api/searchProfile/route.ts
+++ app/api/searchProfile/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { createClient } from '@/utils/supabase/server';
+import { openai } from "@/utils/openai/client";
 
 
 export async function GET(request: Request) {
@@ -9,10 +10,17 @@ export async function GET(request: Request) {
     const supabase = createClient();
 
     const searchQuery = queryParams["search_query"]
+    const queryEmbedding = await openai.embeddings.create({
+      input: searchQuery,
+      model: "text-embedding-3-small",
+    });
 
-    const response = "" //semanticSearch(searchQuery)
-
-    return NextResponse.json({ response }, { status: 200 });
+    const { data: documents } = await supabase.rpc('search_profiles', {
+      query_embedding: JSON.stringify(queryEmbedding.data[0].embedding),
+      match_threshold: -0.99,
+      match_count: 10,
+    })
+    return NextResponse.json({ documents }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
